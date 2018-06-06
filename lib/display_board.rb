e587fe4ab9bf2cda76cf5d6d6d68f28bac077011
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-06 04:46:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,9 @@ def display_board
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   horizontal_divider = "-----------"
   puts "A Tic Tac Toe Board"
-  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} " 
   puts horizontal_divider
-  puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts horizontal_divider
-  puts "#{board[6]}|#{board[7]}|#{board[8]}"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
